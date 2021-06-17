145287702c2f270ae8025ecc10c7d7ec5aa56fb0
Sean Proctor <sproctor@gmail.com>
2021-06-17 06:38:26 -0400

    release 0.3.0

--- signaturepad/build.gradle.kts
+++ signaturepad/build.gradle.kts
@@ -47,7 +47,7 @@ afterEvaluate {
                 // You can then customize attributes of the publication as shown below.
                 groupId = "com.github.sproctor"
                 artifactId = "signaturepad"
-                version = "0.2.0"
+                version = "0.3.0"
             }
         }
     }
