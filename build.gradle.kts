1d2f7f80c27ab00bbeb6df1557448ac42f51274a
Sean Proctor <sproctor@gmail.com>
2026-06-01 08:59:38 -0400

    update to compose 1.11.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@ import com.vanniktech.maven.publish.MavenPublishBaseExtension
 
 plugins {
     alias(libs.plugins.kotlin.multiplatform) apply false
-    alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.kotlin.multiplatform.library) apply false
     alias(libs.plugins.compose) apply false
